9d5f9fe95000a6418a765e1609a8bd69672e5196
rohithaider <rohithaider@gmail.com>
2024-10-07 10:40:37 +0600

    svg component seperated

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,21 +1,6 @@
+import SvgImage from "./Svg";
 import Titles from "./Titles";
-function SvgImage({incomingPath}){
-    const path = incomingPath;
-    return(
-        <div className="relative z-10 mb-10 flex h-[70px] w-[70px] items-center justify-center rounded-[14px] bg-primary">
-                <span className="absolute left-0 top-0 -z-[1] mb-8 flex h-[70px] w-[70px] rotate-[25deg] items-center justify-center rounded-[14px] bg-primary bg-opacity-20 duration-300 group-hover:rotate-45"></span>
-                <svg
-                  width="37"
-                  height="37"
-                  viewBox="0 0 37 37"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  {path}
-                </svg>
-              </div>
-    )
-}
+
 
 function Card({title,description,svgImage}){
     return(
